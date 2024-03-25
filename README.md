28cacb7a1eb32473c7453379d148f45c2abb2b5f
Ashu-Viron <ashutoshra8581@gmail.com>
2024-03-26 04:08:15 +0530

    my

---
+++ README.md
@@ -0,0 +1,4 @@
+Hello ,Everyone welcome to our Project one Apna-Kaam where job seekers meet job providers
+
+- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
+- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
---
+++ src/sidebar/EmploymentType.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import Inputfield from "../components/Inputfield";
+
+
+const EmploymentType = ({handleChange}) => {
+  return (
+    <div>
+    <h4 className="text-lg font-medium mb-2">Type of Employment</h4>
+    <div>
+      <label className="sidebar-label-container">
+        <input
+          type="radio"
+          name="test"
+          id="test"
+          value=""
+          onChange={handleChange}
+        />
+        <span className="checkmark"></span>Any experience
+      </label>
+
+      <Inputfield
+        handleChange={handleChange}
+        value="Temporary"
+        title="Temporary"
+        name="test"
+      />
+      <Inputfield
+        handleChange={handleChange}
+        value="Full-time"
+        title="Full-time"
+        name="test"
+      />
+      <Inputfield
+        handleChange={handleChange}
+        value="Part-time"
+        title="Part-time"
+        name="test"
+      />
+    </div>
+  </div>
+  )
+}
+
+export default EmploymentType
